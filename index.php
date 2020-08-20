1739c4850857ec529e3699e54cf1911ae824fc67
julionsales <69657986+julionsales@users.noreply.github.com>
2020-08-20 07:11:09 -0300

    modal de update e botão de login

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 </head>
 
 <body>
- <!-- teste de botão topoo -->
+ <!--botão topo -->
  <a class="gotopbtn" href="#"> <i class="fas fa-arrow-up"></i> </a>
 
 
@@ -42,6 +42,8 @@
     <thead>
 
       <tr>
+        <th>Orgao
+        </th>
         <th>Setor
         </th>
         <th>
@@ -53,78 +55,106 @@
         <tr>
           <td>
             <select class="selectpicker" id="selectSetor" data-live-search=true>    
+
             </select>
           </td>
-          <td colspan="2"> 
-            <div class="input-group mb-0">
-              <input type="text" id="nome" class="form-control" placeholder="Digite o nome aqui. " disabled>
-            </div>
+          <td>
+            <select class="selectpicker" id="teste" data-live-search=true>    
+            </td>
+            <td colspan="2"> 
+              <div class="input-group mb-0">
+                <input type="text" id="nome" class="form-control" placeholder="Digite o nome aqui. " disabled>
+              </div>
 
-          </td>
+            </td>
 
-        </tr>
+          </tr>
 
-        <!-- TABELA COM OS RAMAIS DE TODO MUNDO-->
+          <!-- TABELA COM OS RAMAIS DE TODO MUNDO-->
 
-        <table class="table" id="tableContatos">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Setor</th>
-              <th scope="col">Nome</th>
-              <th scope="col">Ramal</th>
-            </tr>
+          <table class="table" id="tableContatos">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Setor</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Ramal</th>
+              </tr>
 
-          </thead>
-          <tbody>
-
-
-          </tbody>
-        </table>
-
-
-      </tbody>
-    </table>
-  </div>
-
-  <!-- texto de apresentação -->
-
-  <p id="about" style="font-family:arial-black">
-    <small class="text-muted">
-      Desenvolvido por Julio N. Sales - Setor ATSI
-    </small>
-  </p>
+            </thead>
+            <tbody>
 
 
-
-  <p id="email" style="font-family:arial-black">
-    <b>
-      Para efetuar a atualização de ramais,<br> favor enviar os seguintes dados para o e-mail:<br> atsisuporte@smsub.prefeitura.sp.gov.br<br>
-    </b>
-    <b>
-      Ex.<br>
-      Nome:<br>
-      Ramal:<br>
-      Setor:<br>
-    </b>
-  </p>
-
-</body>
-
-</html>
+            </tbody>
+          </table>
 
 
-<!-- Scripts CDN para buscar jquery bootsrap e popper -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        </tbody>
+      </table>
+    </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- texto de apresentação -->
 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
-
-
-<!-- JS  do select picker -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
-
+    <p id="about" style="font-family:arial-black">
+      <small class="text-muted">
+        Desenvolvido por Julio N. Sales - Setor ATSI
+      </small>
+    </p>
 
 
-<script src="source/script.js"> </script>
+    <!--  modal para efetuar a solicitacao de alteracao de ramais--> 
+    <div id="update">
+      <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Atualizar Ramal</button> 
+    </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Solicite aqui a Alteracao Desejada</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="recipient-name" class="col-form-label">Email:</label>
+                <input type="text" class="form-control" id="recipient-name">
+              </div>
+              <div class="form-group">
+                <label for="message-text" class="col-form-label">Oque deseja alterar:</label>
+                <textarea class="form-control" id="message-text"></textarea>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+            <button type="button" class="btn btn-success">Enviar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- botao para efetuar o login e visualizar o crud -->
+    <div id="login">
+      <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Login</button>
+    </div>
+  </body>
+
+  </html>
+
+
+  <!-- Scripts CDN para buscar jquery bootsrap e popper -->
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+
+  <!-- JS  do select picker -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+
+
+
+  <script src="source/script.js"> </script>
